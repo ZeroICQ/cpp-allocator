1f171dc9045a7cee70e4e20920004e14703fe1f1
Alexey Mogilyovkin <zeroicq@gmail.com>
2018-06-15 23:41:01 +1000

    Add allocator API to header

--- src/fixed_allocator.h
+++ src/fixed_allocator.h
@@ -1,1 +1,22 @@
 #pragma once
+
+namespace atl {
+
+template <class T>
+class allocator {
+public:
+    using value_type = T;
+
+//    using propagate_on_container_move_assignment = std::true_type;
+//    using is_always_equal = std::true_type;
+
+    allocator() noexcept;
+    allocator(const allocator&) noexcept;
+    template <class U> allocator(const allocator<U>&) noexcept;
+
+    ~allocator();
+    T* allocate(size_t n);
+    void deallocate(T* p, size_t n);
+};
+
+}//namespace atl

275c70591600cbfbac2c9769c3208d929c3ee814
Alexey Mogilyovkin <zeroicq@gmail.com>
2018-06-16 11:44:28 +0000

    Save

--- src/fixed_allocator.h
+++ src/fixed_allocator.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <assert.h>
 
 namespace atl {
 
@@ -16,27 +17,55 @@ public:
 
     ~allocator();
     T* allocate(size_t n);
-    void deallocate(T* p, size_t n);
+    void deallocate(T* pointer, size_t n);
 
 private:
-    static constexpr size_t MAX_MEMORY = 2147483648;//in bytes. now 2gb
-    std::vector<T*> free_;
-    std::vector<T*> allocated_;
+    static constexpr size_t MAX_MEMORY = 104857600;//in bytes. now 100mb
+    size_t memory_used_;
+    size_t max_size_;
     T* memory_block_;
 
+    std::vector<T*> free_;
 };
 
 template<class T>
 allocator<T>::allocator() noexcept
 {
-    auto max_size = MAX_MEMORY / sizeof(T);
-    memory_block_ = new T[]
+    max_size_ = MAX_MEMORY / sizeof(T);
+    memory_used_ = max_size_ * sizeof(T);
+    //ASK: this vs malloc
+    memory_block_ = reinterpret_cast<T*>(new char[memory_used_]);
+
+    free_.reserve(max_size_);
+
+    for (size_t i = 0; i < max_size_; i++) {
+        free_.push_back(memory_block_ + i);
+    }
 }
 
 template<class T>
 allocator<T>::~allocator()
 {
     delete[] memory_block_;
+}
+
+template<class T>
+T* allocator<T>::allocate(size_t n)
+{
+    if (n > 1 || free_.empty()) {
+        throw std::bad_alloc();
+    }
+
+    T* pointer_to_allocated_memory = free_.back();
+    free_.pop_back();
+    return  pointer_to_allocated_memory;
+}
+
+template<class T>
+void allocator<T>::deallocate(T* pointer, size_t n)
+{
+    assert(0 <= n && n <= 1);
+    free_.push_back(pointer);
 }
 
 }//namespace atl
--- tests/allocator_tests.cpp
+++ tests/allocator_tests.cpp
@@ -1,9 +1,37 @@
+#include <list>
 #include "catch.hpp"
 #include "fixed_allocator.h"
 
-TEST_CASE("stub")
+template <class T>
+bool is_same(const std::list<T, atl::allocator<T>>& atl_list, const std::list<T>& std_list)
 {
-    atl::allocator<int> a;
-    REQUIRE(true);
-//    CHECK(false);
+    if (atl_list.size() != std_list.size()) {
+        return false;
+    }
+
+    auto it_a = atl_list.begin();
+    auto it_s = std_list.begin();
+
+    for (;it_a != atl_list.end(); it_a++, it_s++) {
+        if (*it_a != *it_s) {
+            return false;
+        }
+    }
+    return true;
+}
+
+template <class T>
+void copy(std::list<T, atl::allocator<T>>& atl_list, const std::list<T> std_list)
+{
+    for (auto it = std_list.begin(); it != std_list.end(); it++) {
+        atl_list.push_back(*it);
+    }
+}
+
+TEST_CASE("test int")
+{
+    std::list<int> std_list = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    std::list<int, atl::allocator<int>> alloc_list;
+    copy(alloc_list, std_list);
+    REQUIRE(is_same(alloc_list, std_list));
 }
